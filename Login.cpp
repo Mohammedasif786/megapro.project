eb4022866636193e7d737d7d82ed7a15032e7b1e
Mohammed asif <90496234+Mohammedasif786@users.noreply.github.com>
2023-04-18 05:43:17 -0700

    Login and Reg
    On Going...

---
+++ Login.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+#include "D:\vim\vim90\easy.h"
+#define ip out
+#define op in
+using std::cout; using std::cin; using std::string;
+int main(void) {
+   std::ofstream ok("Login.txt",std::ios::ip);//?
+      
+//!Ending point
+    ok.close();
+    return EXIT_SUCCESS;
+}
